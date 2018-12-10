f6d2d9aa72f200337a63b9f54b4ee0db38badcfe
Brett Nelson <Hockeybud2011@hotmail.ca>
2018-12-10 09:37:39 -0600

    Optimized the images

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
 
 
 gulp.task('default', () => {
@@ -11,4 +12,10 @@ gulp.task('default', () => {
     }))
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('image-min', () => {
+    gulp.src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'))
 });
