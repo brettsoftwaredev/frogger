479e4fce5b75d0379e68157d84439b3ccc49eb78
Brett Nelson <Hockeybud2011@hotmail.ca>
2018-12-10 09:26:22 -0600

    minified the CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
 
 gulp.task('default', () => {
     gulp.src('src/css/*.css')
@@ -7,5 +9,6 @@ gulp.task('default', () => {
         browsers: ['last 2 versions'],
         cascade: false
     }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist/css'))
 });
